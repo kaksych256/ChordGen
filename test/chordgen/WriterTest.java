134938b7ed13cbb08b1f4704e6365fe66176096e
Алекс <@Aleks>
2016-05-12 21:39:10 +0700

    Проба JUnit

---
+++ test/chordgen/WriterTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chordgen;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Алекс
+ */
+public class WriterTest {
+    
+    public WriterTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        System.out.println("* WriterTest: setUp() method");
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+        System.out.println("* WriterTest: tearDown() method");
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
