005881301e71a7d44055c6f70e5a8ca8c0d7c6e8
piastu <piastu@github.com>
2016-05-18 10:55:41 +0700

    CGSingleton

--- src/chordgen/CGSingleton.java
+++ src/chordgen/CGSingleton.java
@@ -10,6 +10,42 @@ package chordgen;
  *
  * @author Алекс
  */
-public class CGSingleton {
-    
+import jm.music.data.Phrase;
+import jm.music.data.Score;
+
+public class CGSingleton implements CGFactory {
+    private static CGSingleton object = null;
+	
+	public static CGSingleton getFactory() {
+		
+		if (object == null) {
+			object = new CGSingleton();
+		}
+		return object;
+		
+	}
+        
+        private CGSingleton() {
+        }
+        
+    @Override
+    public NoteArray getNoteArray() {
+        return (NoteArray) new NoteArray();
+    }
+
+    @Override
+    public GenerateMelody getGenerateMelody() {
+        return (GenerateMelody) new GenerateMelody();
+    }
+
+    @Override
+    public Phrase getPhrase() {
+        return (Phrase) new Phrase();
+    }
+
+    @Override
+    public Score getScore() {
+        return (Score) new Score();
+    }
+        
 }
