fbc413b76af0cc6d599802cfac9e21e90ed7594a
piastu <piastu@github.com>
2016-05-18 10:49:57 +0700

    NoteWrapper

--- src/chordgen/NoteWrapper.java
+++ src/chordgen/NoteWrapper.java
@@ -31,8 +31,43 @@ public class NoteWrapper extends Note {
        int notePitch = note.getPitch();
         String noteOctave = "";
         String strips = "";
+        
         String noteName=null;
-    
-       return noteName + noteOctave + strips + " ";
+        
+        if (notePitch>0)
+        {
+        noteOctave = Integer.toString((notePitch/12)-1);
+        if (notePitch%12==0)
+        {noteName="C";}
+        if (notePitch%12==2)
+        {noteName="D";}
+        if (notePitch%12==4)
+        {noteName="E";}
+        if (notePitch%12==5)
+        {noteName="F";}
+        if (notePitch%12==7)
+        {noteName="G";}
+        if (notePitch%12==9)
+        {noteName="A";}
+        if (notePitch%12==11)
+        {noteName="B";}
+        }
+        
+        else if (notePitch<0)
+        {noteName = "x";}
+        
+        double durNote = note.getDuration();
+        
+        double stripNumber = durNote/0.25;
+        
+        for (int k = 0;k<stripNumber-1;k++)
+        {
+            strips += "-";
+        }
+        
+    return noteName + noteOctave + strips + " ";
+        //String noteP = Integer.toString(note.getPitch());
+        //return noteP;
+        
     }
 }
